3e3dafe2673bb54e051a8b7b602a1b631ae6607c
Krishnendu97Ghosh <krishnendu1997vk@gmail.com>
2025-10-02 17:03:32 +0530

    first commit

---
+++ src/main/java/com/ecs/EcsApp1Application.java
@@ -0,0 +1,13 @@
+package com.ecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecs/EcsApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
